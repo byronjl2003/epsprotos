f451ce5ad3653a472d2ffdb762acba6a66686886
byronjl2003 <byronjl2003@gmail.com>
2020-07-25 16:14:30 +0000

    commit desde GCP

--- src/app/pages/Administracion/rangos/rangos.component.ts
+++ src/app/pages/Administracion/rangos/rangos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalinteractionService } from 'src/app/services/service.index';
 
 @Component({
   selector: 'app-rangos',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RangosComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _modalservice: ModalinteractionService) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  showDialog() {
+    // this.ocultar = false;
+    // console.log(this.ocultar);
+    this._modalservice.changeEstate(false);
   }
+  /* showDialog() {
+    let view = this.modal_1.createEmbeddedView(null);
+    this.vc.insert(view);
+    this.modal_1.elementRef.nativeElement.previousElementSibling.classList
+        .remove('fade');
+    this.modal_1.elementRef.nativeElement.previousElementSibling.classList.add(
+        'modal-open');
+    this.modal_1.elementRef.nativeElement.previousElementSibling.style.display =
+        'block';
+    this.backdrop = document.createElement('DIV');
+    this.backdrop.className = 'modal-backdrop';
+    document.body.appendChild(this.backdrop);
+  } */
 
+  /* closeDialog() {
+    this.vc.clear();
+    document.body.removeChild(this.backdrop);
+  } */
 }
